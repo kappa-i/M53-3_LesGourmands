422d29cd4bb2cf4e935885afc3d3a3fcab6d1e61
kappa-i <gaby.kappai@gmail.com>
2025-05-19 14:44:08 +0200

    jsp

--- src/main/Player.java
+++ src/main/Player.java
@@ -10,15 +10,29 @@ public class Player {
 
     //Méthode
     public void addItem(String item){
-
+        if(!inventory.contains(item)){
+            inventory.add(item);
+            System.out.println(item + " a été ajouté à l'inventaire.");
+        } else {
+            System.out.println(item + " est déjà dans l'inventaire.");
+        }
     }
 
     public void removeItem(String item){
-
+        if(inventory.contains(item)){
+            inventory.remove(item);
+            System.out.println(item + " a été retiré de l'inventaire.");
+        } else {
+            System.out.println(item + " n'est pas dans l'inventaire.");
+        }
     }
 
     public void MoveToLocation(Location location){
-
+        if (!location.) {
+            System.out.println("Vous vous déplacez vers : " + location.getLocationName());
+        } else{
+            System.out.println("La zone est bloquée");
+        }
     }
 
     public void OpenMap(){
