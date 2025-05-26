9be47f6539d83582d19ad1d2c8f4378819ef1b38
kappa-i <gaby.kappai@gmail.com>
2025-05-26 12:06:52 +0200

    gvgv

--- src/main/Command.java
+++ src/main/Command.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package main;
 
 public abstract class Command {
@@ -5,4 +6,26 @@ public abstract class Command {
     private String description;
 
     public void execute();
+=======
+
+public abstract class Command {
+    protected String name;
+    protected String description;
+
+    public Command(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    // Méthode à implémenter dans les sous-classes
+    public abstract void execute(String input);
+>>>>>>> Stashed changes
 }
