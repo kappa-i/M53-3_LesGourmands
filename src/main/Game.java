69eeca69c8d67cf97e6d4919ddc28dc6b64914b8
DidiEko <cindyeko.demont@hes-so.ch>
2025-06-02 13:25:15 +0200

    room

--- src/main/Game.java
+++ src/main/Game.java
@@ -15,17 +15,31 @@ public class Game {
         commandRegistry = new CommandRegistry();
 
         // Création d'une pièce
-        Location room1 = new Location("Aisschiat Street", "Vous êtes à “Aisschiat Street“", false);
-        Location room2 = new Location("Gabwaaay Bay", "Vous êtes à “Gabwaaay Bay“", false);
-        Location room3 = new Location("Dibiril House", "Vous êtes à “Dibiril House“", true);
-        Location room4 = new Location("Savane Gigi", "Vous êtes à “Savane Gigi“", true);
+        Location room1 = new Location("Aisschiat Street", "Vous êtes à Aisschiat Street", false);
+        Location room2 = new Location("Gabwaaay Bay", "Vous êtes à Gabwaaay Bay", false);
+        Location room3 = new Location("Dibiril House", "Vous êtes à Dibiril House", true);
+        Location room4 = new Location("Eko CoffeeShop", "Vous êtes à Eko CoffeeShop", true);
+        Location room5 = new Location("Nuno's Park", "Vous êtes à Nuno's Park", false);
+        Location room6 = new Location("Sackkee Studio", "Vous êtes à Sackkee Studio", true);
+        Location room7 = new Location("Pierre Stone", "Vous êtes à Pierre Stone", true);
+        Location room8 = new Location("Tialoka Arena", "Vous êtes à Tialoka Arena", true);
+        Location room9 = new Location("Vieux Port de Raul", "Vous êtes à Vieux Port de Raul", true);
+        Location room10 = new Location("CilsByCarla", "Vous êtes à CilsByCarla", true);
+        Location room11 = new Location("F2SS Hall", "Vous êtes à F2SS Hall", true);
+        Location room12 = new Location("Alex GameCenter", "Vous êtes à Alex GameCenter", true);
+
 
 
         room1.addItem(new Letter("Letter", "The treasure is east."));
         worldMap.addLocation(room1, 0, 0);
         worldMap.addLocation(room2, 1, 0);
-        worldMap.addLocation(room3, 1, 1);
-        worldMap.addLocation(room4, 1, 2);
+        worldMap.addLocation(room3, 2, 0);
+        worldMap.addLocation(room4, 3, 0);
+        worldMap.addLocation(room5, 4, 0);
+        worldMap.addLocation(room6, 0, 1);
+        worldMap.addLocation(room7, 0, 2);
+        worldMap.addLocation(room8, 0, 3);
+
         worldMap.setPlayerLocation(room1);
 
         
